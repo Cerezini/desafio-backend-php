5c6de866d980d741ebb12e6a719f1f7f4389aa03
Mateus Cerezini <mateuscgomes@gmail.com>
2019-07-24 22:43:07 -0300

    Criar usuário sem validação de dados

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+
 class UsersController extends Controller
 {
     public function createCostumer() {
@@ -12,8 +15,33 @@ class UsersController extends Controller
         return "Test createSeller";
     }
     
-    public function createUser() {
-        return "Test createUser";
+    public function createUser (Request $request)
+    {
+        // Validate the request...
+
+        $userSameCpf = User::where('cpf', $request->cpf)->first();
+
+        if ($userSameCpf != null) {
+            return "ERROR 422";
+        }
+
+        $userSameEmail = User::where('email', $request->email)->first();
+
+        if ($userSameEmail != null) {
+            return "ERROR 422";
+        }
+
+        $user = new User;
+
+        $user->cpf = $request->cpf;
+        $user->email = $request->email;
+        $user->full_name = $request->full_name;
+        $user->password = $request->password;
+        $user->phone_number = $request->phone_number;
+        
+        $user->save();
+    
+        return $user;
     }
 
     public function getUser($id) {
